d128608d77cd3552032b13a2b1e7fcd5d4119cad
guywmustang <43661726+guywmustang@users.noreply.github.com>
2021-09-01 22:27:10 -0700

    0.2: Fixes & improvements

--- Google Photos dark theme.user.js
+++ Google Photos dark theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Google Photos dark theme
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  A darker look
 // @author       You
 // @match        https://photos.google.com/*
@@ -14,25 +14,83 @@
 (function () {
     'use strict';
 
-    var retry = null;
+    function addStyle() {
 
-GM_addStyle(`
+        GM_addStyle(`
+            /* top bar */
+            .QtDoYb {
+            background-color: black;
+            }
 
-/* top bar */
-.QtDoYb {
-  background-color: black;
-}
+            /* background for main page & side bar */
+            body, .wDSX5e {
+            background-color: #252525;
+            }
 
-/* background for main page & side bar */
-body, .wDSX5e {
-  background-color: #252525;
-}
+            /* Main page font color */
+            .xA0gfb, .G7ubtf, .RLo1Hf {
+            color: #dddddd;
+            }
 
-/* Font color changes */
-.xA0gfb, .G7ubtf, .RLo1Hf {
-  color: #dddddd;
-}
+            /* background color on search page */
+            c-wiz.B6Rt6d.zcLWac.eejsDc.G9Yhnf, div.eReC4e.FbgB9 {
+            background-color: #252525;
+            }
 
-` );
+            /* font color on search page */
+            div.ZEmz6b, h2.ZEmz6b, .OgirMe, .hsohWb, div.hsohWb, .ApP0z, div.dykGZb, div.avzd9e, div.R1QzSc.ecU3Ec > div {
+            color: #dddddd;
+            }
+
+            /* View all link on search page */
+            div.dykGZb span {
+                color: #dfdfdf;
+            }
+
+            /* Text color on sharing page */
+            div.I7yCae, div.DNAsC.A1fzDc.bSAKfb a {
+                color: #dfdfdf;
+            }
+
+            a.DOAbib:hover {
+            background-color: yellow;
+            }
+        ` );
+    }
+
+    var oldHref = document.location.href;
+
+    window.onload = function() {
+    
+        var
+             bodyList = document.querySelector("body")
+    
+            ,observer = new MutationObserver(function(mutations) {
+    
+                mutations.forEach(function(mutation) {
+    
+                    if (oldHref != document.location.href) {
+    
+                        oldHref = document.location.href;
+    
+                        /* Changed ! your code here */
+                        setTimeout(addStyle, 250);
+    
+                    }
+    
+                });
+    
+            });
+    
+        var config = {
+            childList: true,
+            subtree: true
+        };
+    
+        observer.observe(bodyList, config);
+    
+    };
+
+    addStyle();
 
 })();
